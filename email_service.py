ea3c1e87eb189d3fd1ca9df1b623b2e7387e3015
Yahir Hernandez <115806774+yayossyam@users.noreply.github.com>
2026-03-12 17:05:59 +0000

    Tercer avance

--- email_service.py
+++ email_service.py
@@ -2,11 +2,21 @@
 import smtplib #Se conecta al servidor del correo
 from email.mime.text import MIMEText #Genera el cuerpo del mensaje
 from email.mime.multipart import MIMEMultipart #Permite usar txt,html,img,etc
+from email.mime.base import MIMEBase 
+from email import encoders
+import os
 
 #Función
-def send_email(send_email, password, recipients, subject, body):
-    smtp_server = "smtp.gmail.com"
-    port = 587
+def send_email(send_email, password, recipients, subject, body, provider="gmail", attachments= None):
+    #Opciones de servidor SMTP
+    if provider.lower() == "gmail":
+        smtp_server = "smtp.gmail.com"
+        port = 587
+    elif provider.lower() == "outlook":
+        smtp_server = "smtp.outlook.com"
+        port = 587
+    else: 
+        raise ValueError("Solo soporta Gmail-Outlook")
 
     #Protección de código
     try:
@@ -19,25 +29,36 @@ def send_email(send_email, password, recipients, subject, body):
         #Cuerpo del mensaje
         message.attach(MIMEText(body, "html"))
 
+        #Adjuntar archivos en caso de existir
+        if attachments:
+            for file_path in attachments:
+                if os.path.exists(file_path):
+                    with open(file_path, "rb") as f:
+                        part = MIMEBase("application", "octet-stream")
+                        part.set_payload(f.read())
+                    encoders.encode_base64(part)
+                    part.add_header(
+                        "Content-Disposition",
+                        f'attachment; filename="{os.path.basename(file_path)}"'
+                    )
+                    message.attach(part)
+                else:
+                    print(f'Archivo no encontrado: {file_path}')
+
         #Conexion  al servidor SMTP
         server = smtplib.SMTP(smtp_server, port)
-
         server.starttls() #Activación TLS Encryption
-
         #Login
         server.login(send_email, password)
-
         #Enviar correo
         server.sendmail(send_email, recipients, message.as_string())
-
         #Cerrar conexion
         server.quit()
 
         #Mensaje de éxito
-        print("Correo enviado de manera exitosa.!")
-
-
+        print(f'Correo enviado con vía {provider.capitalize()}!')
         return True
+
     except Exception as e:
         print("Error al momento de enviar el correo: ",e)
 
--- main.py
+++ main.py
@@ -3,20 +3,27 @@ from email_service import send_email
 
 sender = "y2776430@gmail.com"
 password = "abiglmljfpnjhgmv" #Se debe generar una App Password Gmail.
+#sender = "y2776430@outlook.com"
+#password = "VJVLQPUGQL3N468HP8QU9CUVK"
 
 recipients = [
-    "yahirhernandezolivaress@gmail.com"
+    "yahirhernandezolivaress@gmail.com",
+    "y2776430@outlook.com"
 ]
 
-subject = "Prueba de correo"
+subject = "Reporte de prueba - 12/03/22"
 
 body = """
 <html>
     <body style="font-family: Arial, sans-serif; color: #333333; line-height:1.5;">
-        <h2 style="color:abc;">Envio de correo </h2>
+        <h2 style="color:1a73e8;">Envio de correo </h2>
         <p>El motivo de este comunicado solo es con fines de pruebas</p>
+        <p>Se recuerda <strong>esto es una prueba</strong> con diseño HTML</p>
+        <br>
+        <p>Saludos, equipo</p>
     </body>
 </html>
 """
+attachments = [""]
 
-send_email(sender, password, recipients, subject, body)
+send_email(sender, password, recipients, subject, body, provider="gmail", attachments=attachments)
